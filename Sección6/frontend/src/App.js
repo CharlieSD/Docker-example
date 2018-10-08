0cc4e20a23f3cb991046d1a0d8c733006aad6abe
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-10-08 16:30:43 -0500

    Some changes in src

--- Sección6/frontend/src/App.js
+++ Sección6/frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye there amazing!
+            Is this Change?
           </p>
           <a
             className="App-link"
